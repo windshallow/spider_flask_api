b0588df327e1777e9ad541a959653cdef1c877bd
linrenwei <windstreet12@gmail.com>
2018-10-30 17:00:34 +0800

    调整4

--- proj/celery_app.py
+++ proj/celery_app.py
@@ -2,8 +2,8 @@
 from __future__ import absolute_import
 from celery import Celery
 
-app = Celery('proj', include=['proj.tasks'])
-app.config_from_object('proj.celeryconfig')
+app = Celery('spider_flask_api.proj', include=['spider_flask_api.proj.tasks'])
+app.config_from_object('spider_flask_api.proj.celeryconfig')
 
 if __name__ == '__main__':
     app.start()
